f48c4fc41f4fb6fd5cba30e243d23b8a58e0b2f4
jair1823 <cordero1823.cr@gmail.com>
2020-02-25 16:37:35 -0600

    Listo!

--- src/models/customer.model.ts
+++ src/models/customer.model.ts
@@ -1,4 +1,5 @@
-import {Entity, model, property} from '@loopback/repository';
+import {Entity, model, property, hasMany} from '@loopback/repository';
+import {Order} from './order.model';
 
 @model()
 export class Customer extends Entity {
@@ -15,6 +16,8 @@ export class Customer extends Entity {
   })
   name: string;
 
+  @hasMany(() => Order, {keyTo: 'id_customer'})
+  orders: Order[];
 
   constructor(data?: Partial<Customer>) {
     super(data);
--- src/repositories/customer.repository.ts
+++ src/repositories/customer.repository.ts
@@ -1,16 +1,22 @@
-import {DefaultCrudRepository} from '@loopback/repository';
-import {Customer, CustomerRelations} from '../models';
+import {DefaultCrudRepository, repository, HasManyRepositoryFactory} from '@loopback/repository';
+import {Customer, CustomerRelations, Order} from '../models';
 import {PostsqlDataSource} from '../datasources';
-import {inject} from '@loopback/core';
+import {inject, Getter} from '@loopback/core';
+import {OrderRepository} from './order.repository';
 
 export class CustomerRepository extends DefaultCrudRepository<
   Customer,
   typeof Customer.prototype.id_customer,
   CustomerRelations
 > {
+
+  public readonly orders: HasManyRepositoryFactory<Order, typeof Customer.prototype.id_customer>;
+
   constructor(
-    @inject('datasources.postsql') dataSource: PostsqlDataSource,
+    @inject('datasources.postsql') dataSource: PostsqlDataSource, @repository.getter('OrderRepository') protected orderRepositoryGetter: Getter<OrderRepository>,
   ) {
     super(Customer, dataSource);
+    this.orders = this.createHasManyRepositoryFactoryFor('orders', orderRepositoryGetter,);
+    this.registerInclusionResolver('orders', this.orders.inclusionResolver);
   }
 }
